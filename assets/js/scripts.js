e93b8b99b44bca1d99e50984cdf5c9739795fb9f
giovanaramoscp <giovanaramos.pessoa@gmail.com>
2025-02-16 01:36:34 -0300

    atualizacao-projetos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,10 @@
-const accordionHeaders = document.querySelectorAll(".accordion_header");
+let btnMenu = document.getElementById("btn-menu");
+let menu = document.getElementById("menu-mobile");
 
-accordionHeaders.forEach(header => {
-    header.addEventListener("click", () => {
-        const accordionItem = header.parentElement;
-        const accordionActive = accordionItem.classList.contains("active");
+btnMenu.addEventListener("click", () => {
+  menu.classList.add("abrir-menu");
+});
 
-    accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
-    })
-})
-
-const menuLinks = document.querySelectorAll(".menu_link");
-
- menuLinks.forEach(item => {
-    item.addEventListener("click", ()) => {
-        menuLinks.forEach(i => i.classList.remove("active"));
-        item.classList.add("active");
-    } 
-})
-
-function toggleMenu() {
-    document.querySelector(".menu-mobile").classList.toggle("menu-open");
-  }
+menu.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
